9a11a11bd0c8f151626814d361520b8a85eaf7f4
expirydateproject <expirydateproject@gmail.com>
2025-04-07 23:55:10 +0530

    new profile

--- lib/main.dart
+++ lib/main.dart
@@ -121,27 +121,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Expiry Date Tracker',
-      debugShowCheckedModeBanner: false, // Add this line to remove the debug banner
-      theme: AppTheme.theme,
-      home: AppRoutes.getInitialScreen(),
-      routes: {
-        '/login': (context) => const LoginScreen(),
-        '/signup': (context) => const SignupScreen(),
-        '/forgot-password': (context) => const ForgotPasswordScreen(),
-        '/home': (context) => const HomeScreen(),
-        '/notifications': (context) => const NotificationPage(),
-        '/AddProduct': (context) => const AddProductPage(),
-      },
-    );
-  }
-}
 
 class ErrorApp extends StatelessWidget {
   final String errorMessage;
